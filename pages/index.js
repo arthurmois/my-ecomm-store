489bb0d9e1187b6cdf50ef575222920964dd148a
Arthur Mois <arthurmois@gmail.com>
2021-07-15 14:10:28 -0700

    apostrophe bug

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Arthur's Photos
+          Arthurs Photos
         </h1>
 
         <p className={styles.description}>
